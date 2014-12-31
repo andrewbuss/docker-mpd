5fa0ef7caf8a1ed62e32856a960f43224c516b83
Andrew Buss <abuss@ucsd.edu>
2014-12-30 16:52:27 -0800

    Automatically generate password on first start

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+if ! grep -Fq ^password /etc/mpd.conf
+then
+	MPD_PASSWORD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c10)
+	echo password \"$TERM@read,write,control,admin\" >> /etc/mpd.conf
+	echo MPD has been configured with the password $MPD_PASSWORD
+end
 mpd --no-daemon --stdout -v /etc/mpd.conf
