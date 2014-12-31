8ce616702baa9c9866d5c00c25073cb58682d473
Andrew Buss <abuss@ucsd.edu>
2014-12-30 17:51:23 -0800

    Fixed password permissions

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 if ! grep ^password /etc/mpd.conf
 then 
     MPD_PASSWORD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c10) ;
-    echo password \"$MPD_PASSWORD@read,write,control,admin\" >> /etc/mpd.conf ;
+    echo password \"$MPD_PASSWORD@read,add,control,admin\" >> /etc/mpd.conf ;
     echo MPD has been configured with the password $MPD_PASSWORD ;
 fi
 mpd --no-daemon --stdout -v /etc/mpd.conf
