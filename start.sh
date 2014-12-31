96e5336bd20c5d4a98ee7e77b3b14d7f7a18f9dd
Andrew Buss <abuss@ucsd.edu>
2014-12-30 17:40:21 -0800

    Fixed broken start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if ! grep -Fq ^password /etc/mpd.conf
-then
-	MPD_PASSWORD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c10)
-	echo password \"$TERM@read,write,control,admin\" >> /etc/mpd.conf
-	echo MPD has been configured with the password $MPD_PASSWORD
-end
+if ! grep ^password /etc/mpd.conf
+then 
+    MPD_PASSWORD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c10) ;
+    echo password \"$MPD_PASSWORD@read,write,control,admin\" >> /etc/mpd.conf ;
+    echo MPD has been configured with the password $MPD_PASSWORD ;
+fi
 mpd --no-daemon --stdout -v /etc/mpd.conf
